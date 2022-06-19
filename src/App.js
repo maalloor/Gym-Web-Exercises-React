c14f68bfdc8fe8796e3f7d5d480befad62173abb
Manuel Loor Macias <maalloor@espol.edu.ec>
2022-06-19 02:43:21 -0500

    Adding the structure of the project folders and files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Route, Routes} from 'react-router-dom';
+import { Box } from '@mui/material';
 
 import './App.css';
 
+// COMPONENTS
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+// PAGES
+import Home from './pages/Home';
+import ExerciseDetail from './pages/ExerciseDetail';
+
 const App = () => {
     return (
-        <div> App </div>
+        <Box width="400px" sx={{ width: { xl: '1488px'}}} m="auto">
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/exercise/:id" element={<ExerciseDetail />} />
+            </Routes>
+            <Footer />
+        </Box>
     )
 }
 
